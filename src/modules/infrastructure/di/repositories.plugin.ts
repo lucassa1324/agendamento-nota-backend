0a97184e81252bd31fc7b69a61036bbe04f24f3f
Lucas Sá <lucassa1324@gmail.com>
2026-02-19 16:40:02 -0300

    isolate-appointment-repo-in-plugin

--- src/modules/infrastructure/di/repositories.plugin.ts
+++ src/modules/infrastructure/di/repositories.plugin.ts
@@ -1,21 +1,21 @@
 import { Elysia } from "elysia";
-import { DrizzleBusinessRepository } from "../../business/adapters/out/drizzle/business.drizzle.repository";
+// import { DrizzleBusinessRepository } from "../../business/adapters/out/drizzle/business.drizzle.repository";
 import { DrizzleAppointmentRepository } from "../../appointments/adapters/out/drizzle/appointment.drizzle.repository";
-import { DrizzleServiceRepository } from "../../services/adapters/out/drizzle/service.drizzle.repository";
-import { DrizzleInventoryRepository } from "../../inventory/adapters/out/drizzle/inventory.drizzle.repository";
-import { DrizzleSettingsRepository } from "../../settings/adapters/out/drizzle/settings.drizzle.repository";
-import { DrizzleExpenseRepository } from "../../expenses/adapters/out/drizzle/expense.drizzle.repository";
-import { GalleryDrizzleRepository } from "../../gallery/adapters/out/drizzle/gallery.drizzle.repository";
+// import { DrizzleServiceRepository } from "../../services/adapters/out/drizzle/service.drizzle.repository";
+// import { DrizzleInventoryRepository } from "../../inventory/adapters/out/drizzle/inventory.drizzle.repository";
+// import { DrizzleSettingsRepository } from "../../settings/adapters/out/drizzle/settings.drizzle.repository";
+// import { DrizzleExpenseRepository } from "../../expenses/adapters/out/drizzle/expense.drizzle.repository";
+// import { GalleryDrizzleRepository } from "../../gallery/adapters/out/drizzle/gallery.drizzle.repository";
 import { UserRepository } from "../../user/adapters/out/user.repository";
-import { DrizzlePushSubscriptionRepository } from "../../notifications/adapters/out/drizzle/push-subscription.drizzle.repository";
+// import { DrizzlePushSubscriptionRepository } from "../../notifications/adapters/out/drizzle/push-subscription.drizzle.repository";
 
 export const repositoriesPlugin = new Elysia()
-  .decorate("businessRepository", new DrizzleBusinessRepository())
+  // .decorate("businessRepository", new DrizzleBusinessRepository())
   .decorate("userRepository", new UserRepository())
   .decorate("appointmentRepository", new DrizzleAppointmentRepository())
-  .decorate("serviceRepository", new DrizzleServiceRepository())
-  .decorate("inventoryRepository", new DrizzleInventoryRepository())
-  .decorate("settingsRepository", new DrizzleSettingsRepository())
-  .decorate("expenseRepository", new DrizzleExpenseRepository())
-  .decorate("galleryRepository", new GalleryDrizzleRepository())
-  .decorate("pushSubscriptionRepository", new DrizzlePushSubscriptionRepository());
+  // .decorate("serviceRepository", new DrizzleServiceRepository())
+  // .decorate("inventoryRepository", new DrizzleInventoryRepository())
+  // .decorate("settingsRepository", new DrizzleSettingsRepository())
+  // .decorate("expenseRepository", new DrizzleExpenseRepository())
+  // .decorate("galleryRepository", new GalleryDrizzleRepository())
+  // .decorate("pushSubscriptionRepository", new DrizzlePushSubscriptionRepository());
