dc3a24df95e1fe63703e8646f1c688566d062fca
Lucas Sá <lucassa1324@gmail.com>
2026-02-19 19:59:13 -0300

    chore_enable_controllers

--- src/index.ts
+++ src/index.ts
@@ -91,8 +91,8 @@ const app = new Elysia()
   .group("/api", (api) =>
     api
       .use(businessController)
-    // .use(serviceController)
-    // .use(appointmentController)
+      .use(serviceController)
+      .use(appointmentController)
     // .use(companyController)
     // .use(inventoryController)
     // .use(settingsController)
