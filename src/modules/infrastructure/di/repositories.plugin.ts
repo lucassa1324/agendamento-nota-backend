cfdce3834a9d3506b54e34cf6320d36c500f4fc1
Lucas Sá <lucassa1324@gmail.com>
2026-02-20 15:08:26 -0300

    wire-inventory-repository

--- src/modules/infrastructure/di/repositories.plugin.ts
+++ src/modules/infrastructure/di/repositories.plugin.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 import { DrizzleBusinessRepository } from "../../business/adapters/out/drizzle/business.drizzle.repository";
 import { DrizzleAppointmentRepository } from "../../appointments/adapters/out/drizzle/appointment.drizzle.repository";
 import { DrizzleServiceRepository } from "../../services/adapters/out/drizzle/service.drizzle.repository";
-// import { DrizzleInventoryRepository } from "../../inventory/adapters/out/drizzle/inventory.drizzle.repository";
+import { DrizzleInventoryRepository } from "../../inventory/adapters/out/drizzle/inventory.drizzle.repository";
 import { DrizzleSettingsRepository } from "../../settings/adapters/out/drizzle/settings.drizzle.repository";
 import { UserRepository } from "../../user/adapters/out/user.repository";
 
@@ -11,7 +11,7 @@ export const repositoriesPlugin = new Elysia()
   .decorate("userRepository", new UserRepository())
   .decorate("appointmentRepository", new DrizzleAppointmentRepository())
   .decorate("serviceRepository", new DrizzleServiceRepository())
-  .decorate("inventoryRepository", {} as any)
+  .decorate("inventoryRepository", new DrizzleInventoryRepository())
   .decorate("settingsRepository", new DrizzleSettingsRepository())
   .decorate("expenseRepository", {} as any)
   .decorate("galleryRepository", {} as any)
