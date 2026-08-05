58a84a3f59d97c63943ef97ff898e8a40e3e6d3e
Maciel Suassuna <macielsuassuna14@gmail.com>
2026-08-04 22:16:22 -0300

    chore: remove unused applyCors helper

--- src/index.ts
+++ src/index.ts
@@ -76,41 +76,6 @@ const startServer = () => {
       validateMagicLinkUseCase,
     );
 
-    const applyCors = (response: Response, request: Request): Response => {
-      const origin = request.headers.get("origin");
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:3002',
-        'http://127.0.0.1:3000',
-        'https://app.aurasistema.com.br',
-        'https://app.staging.aurasistema.com.br',
-        'https://aurasistema.com.br'
-      ];
-
-      const isAllowed = allowedOrigins.includes(origin!) ||
-        (origin && origin.match(/http:\/\/.*\.localhost:\d+$/)) ||
-        (origin && (origin.endsWith('.aurasistema.com.br') || origin === 'https://aurasistema.com.br')) ||
-        (origin && origin.endsWith('.vercel.app'));
-
-      if (isAllowed && origin) {
-        const newHeaders = new Headers(response.headers);
-        newHeaders.set("Access-Control-Allow-Origin", origin);
-        newHeaders.set("Access-Control-Allow-Credentials", "true");
-        newHeaders.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
-        newHeaders.set("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie, X-Requested-With, Cache-Control");
-        newHeaders.set("Access-Control-Expose-Headers", "Set-Cookie, set-cookie, Authorization, Cache-Control");
-
-        return new Response(response.body, {
-          status: response.status,
-          statusText: response.statusText,
-          headers: newHeaders
-        });
-      }
-
-      return response;
-    };
-
     console.log("[STARTUP] Criando instância do Elysia...");
 
     const app = new Elysia({
